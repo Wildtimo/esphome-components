5ef0a455d47ce77c0549118f3353e38f946f226f
Wildtimo <114223553+Wildtimo@users.noreply.github.com>
2024-12-22 04:30:45 -0500

    Update HomeAssistantComponent.cpp

--- components/homeassistant_component/HomeAssistantComponent.cpp
+++ components/homeassistant_component/HomeAssistantComponent.cpp
@@ -7,35 +7,23 @@ namespace homeassistant_component {
 static const char* const TAG = "homeassistant.component";
 
 bool HomeAssistantComponent::can_update_from_api() {
-  if (micros() > ignore_update_until_) {
-    ESP_LOGD(TAG, "can_update_from_api: true %s %d", get_entity_id().c_str(), ignore_update_until_);
-    return true;
-  } else {
-    ESP_LOGD(TAG, "can_update_from_api: false %s %d", get_entity_id().c_str(), ignore_update_until_);
-    return false;
-  }
+  ESP_LOGD(TAG, "can_update_from_api: true %s", get_entity_id().c_str());
+  return true;
 }
 
 void HomeAssistantComponent::ignore_api_updates_with_seconds(double seconds) {
-  ESP_LOGD(TAG, "ignore_api_updates_with_seconds: %s %f", get_entity_id().c_str(), seconds);
-  uint32_t delayTime = 1000000; // 1 second delay (in microseconds)
-  ignore_update_until_ = micros() + delayTime;
+  ESP_LOGD(TAG, "ignore_api_updates_with_seconds bypassed: %s %f", get_entity_id().c_str(), seconds);
+  // Do nothing or remove the delay setting.
 }
 
 bool HomeAssistantComponent::can_publish_to_api() {
-  if (micros() > ignore_publish_until_) {
-    ESP_LOGD(TAG, "can_publish_to_api: true %s %d", get_entity_id().c_str(), ignore_publish_until_);
-    return true;
-  } else {
-    ESP_LOGD(TAG, "can_publish_to_api: false %s %d", get_entity_id().c_str(), ignore_publish_until_);
-    return false;
-  }
+  ESP_LOGD(TAG, "can_publish_to_api: true %s", get_entity_id().c_str());
+  return true;
 }
 
 void HomeAssistantComponent::ignore_publish_with_seconds(double seconds) {
-  ESP_LOGD(TAG, "ignore_publish_with_seconds: %s %f", get_entity_id().c_str(), seconds);
-  uint32_t delayTime = 1000000; // 1 second delay (in microseconds)
-  ignore_publish_until_ = micros() + delayTime;
+  ESP_LOGD(TAG, "ignore_publish_with_seconds bypassed: %s %f", get_entity_id().c_str(), seconds);
+  // Do nothing or remove the delay setting.
 }
 
 }  // namespace homeassistant_component
